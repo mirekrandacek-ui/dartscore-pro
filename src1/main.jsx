8ead8a67819a88cb09d0c597b17e75f2561271f1
mirekrandacek-ui <mirek.randacek@gmail.com>
2025-10-01 10:39:56 +0200

    Update main.jsx

--- src1/main.jsx
+++ src1/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { initI18n } from './i18n.js'
+import './app.css'
 
-initI18n().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-})
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
