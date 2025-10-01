422580abc16c28c9c4de128fb6d5753b8d1b2d9f
mirekrandacek-ui <mirek.randacek@gmail.com>
2025-10-01 10:34:36 +0200

    Create rules.js

---
+++ src1/rules.js
@@ -0,0 +1,6 @@
+export const RULES = [
+  {key:'x01', name:'X01 (301/501)', text:`Začínáš na 301/501 a odečítáš k nule. Volitelně Double/Triple Out. 3 šipky na tah, po třetí se přepíná hráč.`},
+  {key:'cricket', name:'Cricket', text:`Cíle: 15–20 a Bull. Otevřeš 3 zásahy (S=1, D=2, T=3). Můžeš soupeře „zavírat“.`},
+  {key:'around', name:'Around the Clock', text:`Házíš postupně 1→20 a Bull. Každé číslo musí padnout (S/D/T).`},
+  {key:'shanghai', name:'Shanghai', text:`Kolo i cíl je stejné číslo (1…20). Body jen za aktuální číslo. Okamžitá výhra, když v jednom kole hodíš S+D+T.`}
+]
